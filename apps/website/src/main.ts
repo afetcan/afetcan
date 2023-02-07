380e49c8c2a2201ababfc591ba5f0ec505561223
productdevbook <hi@productdevbook.com>
2023-02-07 23:28:46 +0300

    chore: add rapor

--- apps/website/src/main.ts
+++ apps/website/src/main.ts
@@ -1,8 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import { createHead } from '@vueuse/head'
+import { createPlausible } from '@huntersofbook/plausible-vue'
 import App from './App.vue'
 import router from './router'
+
+const plausible = createPlausible({
+  init: {
+    domain: 'acildeprem.com',
+    apiHost: 'https://rapor.acildeprem.com',
+    trackLocalhost: false,
+  },
+  settings: {
+    enableAutoOutboundTracking: true,
+    enableAutoPageviews: true,
+  },
+  partytown: false,
+})
 
 const meta = document.createElement('meta')
 meta.name = 'naive-ui-style'
@@ -15,4 +29,5 @@ const app = createApp(App)
 app.use(router)
 app.use(pinia)
 app.use(head)
+app.use(plausible)
 app.mount('#app')
