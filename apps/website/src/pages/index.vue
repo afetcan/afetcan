444be93d5f4837ea676ebb3a4343b2b5bca591e1
productdevbook <hi@productdevbook.com>
2023-02-08 11:37:02 +0300

    chore: update index

--- apps/website/src/pages/index.vue
+++ apps/website/src/pages/index.vue
@@ -30,14 +30,21 @@ useHead({
 
 <template>
   <div class="flex justify-center items-center h-screen w-screen">
-    <div class="w-full max-w-xl grid grid-cols-1 gap-6">
+    <div class="w-full max-w-xl grid grid-cols-1 gap-6 p-4">
       <div class="text-lg">
-        Bu site yakında aktif olacak. Şimdilik aşağıdaki hesapları takip edebilirsiniz. Ayrıca sadece bir özellik olmayacak. Depremle ilgili harika fikiirlerimiz var. Sizlerle paylaşmak için sabırsızlanıyoruz.
-
+        Acil yardım gerektiren insanların yanında olmak ve onlara destek olmak, hepimizin ortak amaçlarından biridir. Deprem
+        gibi afet durumları, insanların hayatlarını ve evlerini kökten değiştirebilir. Bu nedenle, depremin olduğu yerde bulunan
+        insanlara yardım etmek, hem bugün hem de gelecekte onların hayatını kolaylaştırmak için önemlidir.
         <br>
         <br>
-
-        Biz şuanda yazılımda ana isketli oluşturuyoruz.
+        İnsanların yardım için bir bu platform olarak kullanılabilecektir. Şimdilik
+        sosyal medya, Github ve Discord hesaplarınızı takip etmek mümkün olsa da, yakın zamanda açacağımız internet sitesi, daha da fazla insana
+        ulaşmanıza ve yardım konularını daha detaylı şekilde ele almanıza imkan verecektir.
+        <br>
+        <br>
+        Sizin gibi insanlar, depremzedeler için fazlasıyla yardım etmeyi amaçlayan kuruluşlar, toplumun geleceği açısından önem
+        taşır. Bu amaçla hareket eden pek çok insan ve kuruluşun bir araya gelerek, depremzedelerin
+        yardımına koşmasına katkı sağlayacağız.
         <br>
         <br>
         İnşallah yakında aktif olacağız.
