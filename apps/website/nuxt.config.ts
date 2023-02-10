8c5d6c5f8ed73ffb731ef71e747f407c6a5fa5ce
productdevbook <hi@productdevbook.com>
2023-02-10 18:21:12 +0300

    init

--- apps/website/nuxt.config.ts
+++ apps/website/nuxt.config.ts
@@ -3,8 +3,40 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/devtools',
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
+    '@huntersofbook/i18n/nuxt',
+    '@huntersofbook/plausible-nuxt',
+    '@nuxtjs/partytown',
   ],
   tailwindcss: {
     configPath: './tailwind.config.js',
+  },
+  huntersofbookI18n: {
+    languages: ['tr', 'en'],
+  },
+  plausible: {
+    init: {
+      domain: 'acildeprem.cim',
+      apiHost: 'https://rapor.acildeprem.com',
+    },
+    partytown: true,
+  },
+  i18n: {
+    locales: [
+      {
+        code: 'tr',
+        iso: 'tr-TR',
+        name: 'Türkçe',
+        file: 'tr.json',
+      },
+      {
+        code: 'en',
+        iso: 'en-US',
+        name: 'English',
+        file: 'en.json',
+      },
+    ],
+    defaultLocale: 'tr',
+    langDir: 'language/',
   },
 })
