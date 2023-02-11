006116f600cdb82ff05b60967d9b1be5fc6f0376
productdevbook <hi@productdevbook.com>
2023-02-11 16:02:56 +0300

    add head and favicon

--- apps/website/nuxt.config.ts
+++ apps/website/nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { isDevelopment } from 'std-env'
+
 export default defineNuxtConfig({
   modules: [
     '@nuxt/devtools',
@@ -69,6 +71,34 @@ export default defineNuxtConfig({
     tsConfig: {
       include: [
         './shims-vue.d.ts',
+      ],
+    },
+  },
+  sourcemap: !isDevelopment,
+  app: {
+    keepalive: true,
+    head: {
+      viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
+      bodyAttrs: {
+        class: 'overflow-x-hidden',
+      },
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      ],
+      meta: [
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+        // open graph social image
+        { property: 'og:title', content: 'acildeprem' },
+        { property: 'og:description', content: 'It is an open source project that provides uninterrupted service in the event of an emergency earthquake.' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:image', content: 'https://acildeprem/acildeprem-og.png' },
+        { property: 'og:image:width', content: '3800' },
+        { property: 'og:image:height', content: '1900' },
+        { property: 'og:site_name', content: 'acildeprem' },
+        { property: 'twitter:site', content: '@acildepremcom' },
+        { property: 'twitter:card', content: 'summary_large_image' },
       ],
     },
   },
