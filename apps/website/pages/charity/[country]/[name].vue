ca73a85b8f3b0889fcf6e8839aab4bdf136d6d66
productdevbook <hi@productdevbook.com>
2023-02-11 14:51:20 +0300

    feat: add website and donation

--- apps/website/pages/charity/[country]/[name].vue
+++ apps/website/pages/charity/[country]/[name].vue
@@ -8,6 +8,9 @@ const { copy } = useClipboard({ source })
 const route = useRoute()
 const appStore = useAppStore()
 
+const getCountry = computed(() => {
+  return route.params.country
+})
 const copyAccount = (data: any) => {
   copy(data)
   appStore.dispatchNotification({ title: t('global.success'), content: t('global.successCopy'), type: 'success' })
@@ -26,7 +29,7 @@ const { data } = await useAsyncData(() => queryContent<any>('charity/turkiye').w
   </Teleport>
   <NuxtLayout name="charity">
     <div class="pt-4">
-      <NuxtLink to="/charity" class="bg-gray-200 hover:bg-gray-400 h-10 px-4 rounded flex items-center w-full">
+      <NuxtLink :to="`/charity/${getCountry}`" class="bg-gray-200 hover:bg-gray-400 h-10 px-4 rounded flex items-center w-full">
         <div class="icon-[ph--caret-left-bold] w-5 h-5 mr-1" />
         {{ t('charity.allCharities') }}
         <div class="icon-[ph--heart-duotone] w-5 h-5 ml-auto" />
@@ -43,7 +46,23 @@ const { data } = await useAsyncData(() => queryContent<any>('charity/turkiye').w
           <div class="mt-4 opacity-60">
             Toplam banka hesap sayısı: {{ item.bank.length ? item.bank.length : 0 }}
           </div>
+          <div class="grid grid-cols-2 gap-4 opacity-50 hover:opacity-100">
+            <a :href="item.websiteUrl" target="_blank" class="bg-gray-200 hover:bg-gray-400 h-10 px-4 rounded flex items-center w-full mt-4">
+              <div class="icon-[ph--browser-duotone] w-5 h-5 mr-1" />
+              {{ t('charity.website') }}
+              <div class="icon-[ph--link] w-5 h-5 ml-auto" />
+            </a>
+            <a
+              :href="item.donationUrl" target="_blank"
+              class="bg-gray-200 hover:bg-gray-400 h-10 px-4 rounded flex items-center w-full mt-4"
+            >
+              <div class="icon-[ph--database-duotone] w-5 h-5 mr-1" />
+              {{ t('charity.donation') }}
+              <div class="icon-[ph--link] w-5 h-5 ml-auto" />
+            </a>
+          </div>
         </div>
+
         <div class="grid grid-cols-1 gap-6 mt-10">
           <div v-for="bankItem in item.bank" :key="bankItem.id" class="space-y-4 mb-8">
             <h3 class="mb-4 text-base font-semibold bg-gray-200 py-3 px-2">
